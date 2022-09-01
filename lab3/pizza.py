3a88ce8e193285f8dfb1e9ad16f2c01db7641f13
Natanael Azevedo <78879268+Natanael-Azevedo@users.noreply.github.com>
2022-08-31 22:19:34 -0300

    lab3

---
+++ lab3/pizza.py
@@ -0,0 +1,9 @@
+pizzas = ['calabressa','frango com catupiry','sertaneja']
+#print o nome das pizzas
+for x in pizzas:
+    print(x)
+#print uma frase sobre cada uma das pizzas
+for x in pizzas:
+    print(f'Eu gosto muito da pizza de '+ x)
+
+print('Eu realmente amo pizza')
---
+++ lab3/twenty.py
@@ -0,0 +1,9 @@
+numbers = list(range(1,1000001))
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+odd_numbers = list(range(0,20))
+for x in odd_numbers:
+    if x%2 != 0:
+        print(x)
