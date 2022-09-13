351d972c8b6481a55017cac459503c30c724d2c3
Natanael Azevedo <78879268+Natanael-Azevedo@users.noreply.github.com>
2022-09-13 17:31:05 -0300

    new commit

--- 12_09/q3.py
+++ 12_09/q3.py
@@ -1,16 +1,18 @@
 '''Escreva um programa que recebe como entrada uma sequência de 10 valores quaisquer. Seu programa deverá informar qual o maior e qual o menor  valor informado, além do MDC (máximo divisor comum) para estes dois valores. '''
-lista =[]
-for x in range(10):
-    lista.append(int(input()))   
+lista = [255, 366, 201, 10, 15, 85, 998, 500, 144, 15]
+# for x in range(10):
+#   lista.append(int(input()))
 lista.sort()
-menor=lista[0]
-maior=lista[-1]
-resto = maior%menor
-while(resto!=0):
-    maior = menor
-    menor = resto
-    resto = maior%menor
-    print(f'{maior} {menor} {resto}')
+menor = lista[0]
+maior = lista[-1]
 
-print(f'{maior} {menor} {resto}')
-#255 366 201 10 15 85 998 500 144 5
+x=1
+for x in lista:
+    if maior%x==0 and menor % x == 0:
+        print(f'{maior} {menor} {x}')
+        break
+    else:
+        x = x+1
+
+print(f'{maior} {menor} {x}')
+# 255 366 201 10 15 85 998 500 144 5
--- 12_09/teste.py
+++ 12_09/teste.py
@@ -1,8 +1,17 @@
-lista = [1,2,3,6,7,8,9]
-i=1
-for x in lista: 
-    if x > i:
-        print(f'dia {x}')
+lista = [255, 366, 201, 10, 15, 85, 998, 500, 144, 5]
+# for x in range(10):
+#   lista.append(int(input()))
+lista.sort()
+menor = lista[0]
+maior = lista[-1]
+nova_lista=[maior,menor]
+
+for x in lista:
+    if nova_lista % x == 0:
+        print(f'{maior} {menor} {x}')
         break
     else:
-        i=i+1
+        x = x+1
+
+print(f'{maior} {menor} {x}')
+# 255 366 201 10 15 85 998 500 144 5
